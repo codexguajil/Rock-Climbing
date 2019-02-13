598698e9f9a99022cf19435740e555a0d8a67510
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-02-13 15:40:36 -0700

    Update search test defaults

--- src/Search.test.js
+++ src/Search.test.js
@@ -17,17 +17,27 @@ describe('Search', () => {
     );
   });
 
+  it('should have the proper default state', () => {
+
+    expect(wrapper.state()).toEqual({ value: '' })
+
+  });
+
   it('should invoke handleSearch when button is clicked', () => {
+
     wrapper.find('.search-submit').simulate('click', {preventDefault: () => {}})
     
     expect(handleSearchMock).toBeCalled();
+
   });
 
   it('should update the input value on input change', () => {
+
     expect(wrapper.state('value')).toEqual('');
     
     wrapper.find('#search-input').simulate('change', {target: {value: 'my search input'}})
     
     expect(wrapper.state('value')).toEqual('my search input');
+    
   })
 });

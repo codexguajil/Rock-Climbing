54879b955a6548b8d0e11a12c20cacf11ca3f2b4
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-02-13 15:45:25 -0700

    fix error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
       areas: coloradoClimbingAreas,
       distances: distanceFromDenverGroupings,
       filterAreas: [],
-      filterTypes: []
+      filterTypes: [],
       searchString: ''
     }
   }

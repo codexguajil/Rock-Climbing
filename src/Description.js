151d3c953c151e69d7bb90e6437b7fd67fc99eb9
tim miller <tmiller12@gmail.com>
2019-02-13 16:05:01 -0700

    initial description setup

--- src/Description.js
+++ src/Description.js
@@ -3,15 +3,38 @@ import React, { Component } from 'react';
 export default class Description extends Component {
   constructor() {
     super();
-  }
+    // create state to toggle show no-show
+    this.state = {
+        showClimb: false
+      }
+    // bind it to 'this' component 
+    // this.toggleClimb = this.toggleClimb.bind(this);
+  };
 
-  //toggle display description here
+  // method to toggle climb
+  toggleClimb() {
+    this.setState({
+      showClimb: !this.state.showClimb
+    })
+  };
 
+// render to page 
   render() {
+    // let icon = ' + ';
+
+    // if(this.state.showClimb) {
+    //   icon = ' - ';
+    // }
+// return these props 
     return (
       <div>
-        hiking trail to pick
-      </div>
-    )
+        // Climb Name:
+        // Type:
+        // Grade:
+        // Location:
+        // Distance:
+        // Link:
+      </div>    
+    );
   }
 }

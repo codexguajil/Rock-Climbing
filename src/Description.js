f11e7a06c52d49d52bc765a250101ade9e98fea0
tim miller <tmiller12@gmail.com>
2019-02-14 14:51:47 -0700

    description component set up

--- src/Description.js
+++ src/Description.js
@@ -3,15 +3,31 @@ import React, { Component } from 'react';
 export default class Description extends Component {
   constructor() {
     super();
+    this.state = {
+      showDescription: false
+
+    }
+  }
+  
+
+  toggleDescription = () =>  {
+    this.setState({showDescription: !this.state.showDescription })
   }
 
-  //toggle display description here
-
   render() {
+
     return (
-      <div>
-        <p>Hello</p>
-      </div>
+      <li onClick={this.toggleDescription}>
+        <p> {this.props.matchedType.name} </p>
+        
+        { this.state.showDescription && 
+        <div>  
+        <p>{this.props.matchedType.grade}</p>
+        <p> {this.props.matchedType.type}</p>
+        <p> {this.props.matchedType.link}</p>
+        </div> 
+      }
+      </li>
     )
   }
 }

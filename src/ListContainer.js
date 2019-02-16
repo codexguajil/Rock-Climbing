a60822ade9099eeb46e85c1ef1a202268288bec4
“Kimberly <kimberly.achkio@gmail.com>
2019-02-16 14:23:23 -0700

    Update props naming

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -5,14 +5,14 @@ export default function ListContainer (props) {
 
   let matchedType;
 
-  if(props.areaType.length > 0) {
+  if(props.filterTypes.length > 0) {
     let matchedAreas = Object.keys(props.areasAndRoutes).reduce((acc, area) => {
-      if (props.areaName.includes(area)) {
+      if (props.filterAreas.includes(area)) {
        acc.push(props.areasAndRoutes[area])
       }
       return acc;
     }, [])
-    matchedType = props.areaType.reduce((acc, selectedType) => {
+    matchedType = props.filterTypes.reduce((acc, selectedType) => {
       matchedAreas.forEach((uniqueArea) => {
         uniqueArea.classicRoutes.forEach((route) => {
           if (route.type.includes(selectedType)) {
@@ -26,7 +26,7 @@ export default function ListContainer (props) {
   } else {
     //if the user types an area in the search directly then execute the following:
     matchedType = Object.keys(props.areasAndRoutes).reduce((acc, area) => {
-      if (props.areaName === area) {
+      if (props.filterAreas === area) {
        acc = props.areasAndRoutes[area].classicRoutes
       }
       return acc;

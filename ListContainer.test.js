c42034c8c592b604ad903fc9a4910c9fa874faec
“Kimberly <kimberly.achkio@gmail.com>
2019-02-16 10:26:08 -0700

    Setup list container test

---
+++ ListContainer.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ListContainer from './ListContainer';
+import { shallow } from 'enzyme';
+
+
+describe('Search', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(
+            <ListContainer />
+        );
+    });
+
+});

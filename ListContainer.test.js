5abc1140042b3d0f1f8287977b6796f23a97f4ce
“Kimberly <kimberly.achkio@gmail.com>
2019-02-17 14:04:38 -0700

    Modify wrapper statement

--- ListContainer.test.js
+++ ListContainer.test.js
@@ -52,24 +52,21 @@ const mockColoradoClimbingAreas = {
     }
 
 
-describe('Search', () => {
+describe('ListContainer', () => {
 
-    let wrapper;
+    const wrapper = shallow(
+        <ListContainer filterAreas={mockFilterAreas}
+                        areasAndRoutes={mockColoradoClimbingAreas}
+                        filterTypes={mockFilterTypes}/>
+    );
 
-    beforeEach(() => {
-        wrapper = shallow(
-            <ListContainer filterAreas={mockFilterAreas}
-                            areasAndRoutes={mockColoradoClimbingAreas}
-                            filterTypes={mockFilterTypes}/>
-        );
-    });
 
     it('should produce a collection of climbing routes if user selects type & area', () => {
-        expect()
+        expect(wrapper).toMatchSnapshot(); 
     });
 
-    it('should produce a list of climbing routes that are within the area that the user types in the search', () => {
-        expect()
-    })
+   // it('should produce a list of climbing routes that are within the area that the user types in the search', () => {
+   //     expect()
+    //})
 
 });

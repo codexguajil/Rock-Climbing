020fead9c51aec98ea891b49ab797637c3d58d7c
tim miller <tmiller12@gmail.com>
2019-02-19 16:02:40 -0700

    issues with local filter test file

---
+++ src/Filter.test.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import Filter from './Filter';
+import { shallow } from 'enzyme';
+
+  const mockColoradoClimbingAreas = {
+    "Canon City": {
+        "milesFromDenver": 119,
+        "id": 34,
+        "typesOfClimbing": ["trad", "sport", "toprope", "boulder"],
+        "classicRoutes": [
+            {
+                "name": "Enterprise",
+                "id": 35,
+                "grade": "5.09+",
+                "type": ["sport"],
+                "link": "https://www.mountainproject.com/route/105824687/enterprise"
+            },
+            {
+                "name": "Trick-or-Treat",
+                "id": 42,
+                "grade": "5.11a/b",
+                "type": ["toprope"],
+                "link": "https://www.mountainproject.com/route/105759012/trick-or-treat"
+            }
+        ]
+    },
+    "Boulder Canyon": {
+        "milesFromDenver": 31.5,
+            "id": 12,
+            "typesOfClimbing": ["trad", "sport", "toprope", "boulder", "aid"],
+            "classicRoutes": [
+                {
+                    "name": "North Face Center",
+                    "id": 13,
+                    "grade": "5.07+",
+                    "type": ["trad"],
+                    "link": "https://www.mountainproject.com/route/105748093/north-face-center"
+                },
+                {
+                    "name": "Empor",
+                    "id": 14,
+                    "grade": "5.07+",
+                    "type": ["trad"],
+                    "link": "https://www.mountainproject.com/route/105748142/empor"
+                }
+            ]
+        }
+    }
+
+
+  const mockFunction = jest.fn();
+
+
+
+
+
+
+describe('Filter', () => {
+
+  let wrapper;
+
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Filter climb={mockColoradoClimbingAreas}
+                    updateFromFilter={mockFunction}/>
+      )
+  })
+
+  it('should test the states', () => {
+    expect(wrapper.state()).toEqual({setMax: 50, types: [], areas: [], areasPick: [], typesPick: []
+    })
+  })
+
+  it('should change type', () => {
+    expect(wrapper.state('setMax')).toEqual(50);
+    expect(wrapper.state('types')).toEqual([]);
+    wrapper.find('.range').simulate('change', {target: {value: 120}});
+    expect(wrapper.state('setMax')).toEqual(120);
+    expect(wrapper.state('types')).toEqual(["trad", "sport", "toprope", "boulder", "aid"]);
+  })
+
+  it('should change Area', () => {
+    expect(wrapper.state('typesPick')).toEqual([]);
+    expect(wrapper.state('areas')).toEqual([]);
+    wrapper.find('.range').simulate('change', {target: {value: 120}});
+    wrapper.find('.types-checkbox').at(0).simulate('change', {target: {checked: true}})
+    expect(wrapper.state('typesPick')).toEqual(["trad", "sport", "toprope", "boulder", "aid"]);
+    expect(wrapper.state('areas')).toEqual(['Canon City','Boulder Canyon']);
+  })
+
+  // it('should Update Area', () => {
+  //   // areasPick
+  // })
+
+
+  it('render with snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
